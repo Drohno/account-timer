ec1262861401efe084f9bf5350b1b91ef8dd0c9b
David RN <david.roldan@atmira.com>
2020-10-20 10:06:49 +0200

    angular app

---
+++ src/app/logged.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggedGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    // TODO: Check if user token is correct and if so let him acces the timer
+    const hasAccess = true;
+    return hasAccess;
+  }
+}
---
+++ src/app/timer/timer.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { TimerRoutingModule } from './timer-routing.module';
+import { TimerComponent } from './timer.component';
+
+@NgModule({
+  declarations: [TimerComponent],
+  imports: [CommonModule, TimerRoutingModule, TimerComponent],
+})
+export class TimerModule {}
