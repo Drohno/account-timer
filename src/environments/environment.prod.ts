d03bfdd24e4e20bcb29e361cb45efd6de7a22afa
David RN <david.roldan@atmira.com>
2020-10-20 17:33:27 +0200

    chore: firebase build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyASgub_op-h6VBGadEF2UZiGdF8AlgqzAE',
+    authDomain: 'account-timer.firebaseapp.com',
+    databaseURL: 'https://account-timer.firebaseio.com',
+    projectId: 'account-timer',
+    storageBucket: 'account-timer.appspot.com',
+    messagingSenderId: '342660067744',
+    appId: '1:342660067744:web:23f47daf9e5f75119074c5',
+    measurementId: 'G-CR7HDB0VQ4'
+  }
 };
